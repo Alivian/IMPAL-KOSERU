2cb46d62f991d8c24030f5998feac14fb8f09c05
alivian <sundaynism@gmail.com>
2018-09-25 23:25:43 +0700

    DESAIN

---
+++ src/RunProgram.java
@@ -0,0 +1,25 @@
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class RunProgram {
+
+    public static void main(String[]noobs){
+        try{
+            UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+        } catch(ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e){
+            System.out.println(e.getMessage());
+        }
+        new Login().setVisible(true);
+        }   
+}
